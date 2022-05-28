43521bf42bae9c2ddc66869126f6f910e44fb95a
tatavarthi tarun <tatavarthitarun@gmail.com>
2022-05-28 13:54:49 +0530

    Lesson 4 : Lists and animations

--- app/src/main/java/com/tatav/composelearnings/MainActivity.kt
+++ app/src/main/java/com/tatav/composelearnings/MainActivity.kt
@@ -1,75 +1,244 @@
 package com.tatav.composelearnings
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.skydoves.landscapist.glide.GlideImage
+import com.tatav.composelearnings.ui.theme.ComposeLearnings
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //call composable function only in setContent
         setContent {
-            //calling composable function
-            MessageCard(Message("Brian Tracy","Eat That Frog! Action Workbook: 21 Great Ways to Stop Procrastinating and Get More Done in Less Time","https://images-na.ssl-images-amazon.com/images/I/51ZsCYE0pEL._SX355_BO1,204,203,200_.jpg","https://images.gr-assets.com/authors/1258058862p8/22033.jpg"))
+            // theme for compose
+            ComposeLearnings {
+                //calling composable function
+                Conversation(
+                   SampleData.conversationSample
+                )
+            }
         }
     }
 }
 
 //sample data class
-data class Message(val Author: String,val book:String,val BookImage:String,val AuthorImage:String)
+data class Messages(
+    val Author: String,
+    val book: String,
+    val BookImage: String,
+    val AuthorImage: String
+)
 
 //composable function
 @Composable
-fun MessageCard(message: Message) {
+fun MessagesCard(message: Messages) {
     //Row: To display elements in horizontal layout
     Row(modifier = Modifier.padding(all = 8.dp)) {
         GlideImage(
             imageModel = message.BookImage,
-                    modifier = Modifier
-                        // Set image size to 40 dp
-                        .width(100.dp)
-                        .height(150.dp)
-                // Clip image to be shaped as a circle
-                //.clip(CircleShape)
+            modifier = Modifier
+                // Set image width and height
+                .width(120.dp)
+                .height(200.dp)
         )
 
-    // Add a horizontal space between the image and the column
-    Spacer(modifier = Modifier.width(8.dp))
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
 
-    //Column: Used to arrange elements vertically
-    Column {
-        Text(text = message.book)
-        Text(text ="by ")
-        Row(modifier = Modifier.padding(all = 8.dp), verticalAlignment = Alignment.CenterVertically) {
-            GlideImage(
-                imageModel = message.AuthorImage,
-                modifier = Modifier
-                    // Set image size to 40 dp
-                    .size(40.dp)
-                // Clip image to be shaped as a circle
-                .clip(CircleShape)
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = message.Author)
+        //Column: Used to arrange elements vertically
+        Column {
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 10.dp) {
+                Text(
+                    text = message.book,
+                    color = MaterialTheme.colorScheme.primary,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(all = 4.dp),
+                )
+            }
+            Text(text = "by ", style = MaterialTheme.typography.bodySmall)
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 10.dp) {
+                Row(
+                    modifier = Modifier.padding(all = 8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    GlideImage(
+                        imageModel = message.AuthorImage,
+                        modifier = Modifier
+                            // Set image size to 40 dp
+                            .size(60.dp)
+                            // Clip image to be shaped as a circle
+                            .clip(CircleShape)
+                            //To give border
+                            .border(1.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+                    )
+                    Spacer(modifier = Modifier.width(8.dp))
+                    Text(text = message.Author, color = MaterialTheme.colorScheme.secondary)
+                }
+            }
         }
-
-
-    }}
+    }
 }
 
 //calling composable function for preview in only android studio
-@Preview
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
 @Composable
-fun PreviewMessageCard() {
-   // MessageCard(Message("Robin Sharma","The Monk Who Sold His Ferrari"))
+fun PreviewMessagesCard() {
+    // theme for compose
+    ComposeLearnings {
+        //calling composable function
+        MessagesCard(
+            Messages(
+                "Brian Tracy",
+                "Eat That Frog! Action Workbook: 21 Great Ways to Stop Procrastinating and Get More Done in Less Time",
+                "https://images-na.ssl-images-amazon.com/images/I/51ZsCYE0pEL._SX355_BO1,204,203,200_.jpg",
+                "https://images.gr-assets.com/authors/1258058862p8/22033.jpg"
+            )
+        )
+    }
+}
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
+}
+@Composable
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodySmall
+            )
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+   }
+data class Message(val author:String,val body:String)
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message(
+            "Colleague",
+            "Test...Test...Test..."
+        ),
+        Message(
+            "Colleague",
+            "List of Android versions:\n" +
+                    "Android KitKat (API 19)\n" +
+                    "Android Lollipop (API 21)\n" +
+                    "Android Marshmallow (API 23)\n" +
+                    "Android Nougat (API 24)\n" +
+                    "Android Oreo (API 26)\n" +
+                    "Android Pie (API 28)\n" +
+                    "Android 10 (API 29)\n" +
+                    "Android 11 (API 30)\n" +
+                    "Android 12 (API 31)\n"
+        ),
+        Message(
+            "Colleague",
+            "I think Kotlin is my favorite programming language.\n" +
+                    "It's so much fun!"
+        ),
+        Message(
+            "Colleague",
+            "Searching for alternatives to XML layouts..."
+        ),
+        Message(
+            "Colleague",
+            "Hey, take a look at Jetpack Compose, it's great!\n" +
+                    "It's the Android's modern toolkit for building native UI." +
+                    "It simplifies and accelerates UI development on Android." +
+                    "Less code, powerful tools, and intuitive Kotlin APIs :)"
+        ),
+        Message(
+            "Colleague",
+            "It's available from API 21+ :)"
+        ),
+        Message(
+            "Colleague",
+            "Writing Kotlin for UI seems so natural, Compose where have you been all my life?"
+        ),
+        Message(
+            "Colleague",
+            "Android Studio next version's name is Arctic Fox"
+        ),
+        Message(
+            "Colleague",
+            "Android Studio Arctic Fox tooling for Compose is top notch ^_^"
+        ),
+        Message(
+            "Colleague",
+            "I didn't know you can now run the emulator directly from Android Studio"
+        ),
+        Message(
+            "Colleague",
+            "Compose Previews are great to check quickly how a composable layout looks like"
+        ),
+        Message(
+            "Colleague",
+            "Previews are also interactive after enabling the experimental setting"
+        ),
+        Message(
+            "Colleague",
+            "Have you tried writing build.gradle with KTS?"
+        ),
+    )
 }
