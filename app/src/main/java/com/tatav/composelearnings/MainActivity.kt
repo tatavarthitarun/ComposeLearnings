a45cdfb9ad986846f2cd7db071adf9caa33f599d
tatavarthi tarun <tatavarthitarun@gmail.com>
2022-05-28 18:29:12 +0530

    State in compose

--- app/src/main/java/com/tatav/composelearnings/MainActivity.kt
+++ app/src/main/java/com/tatav/composelearnings/MainActivity.kt
@@ -3,13 +3,66 @@ package com.tatav.composelearnings
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.tatav.composelearnings.ui.theme.ComposeLearnings
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //call composable function only in setContent
         setContent {
+            ComposeLearnings {
+                MyApp()
+            }
+        }
+    }
 
+    @Composable
+    fun MyApp(names: List<String> = listOf("World", "Compose")) {
+        Column(Modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
+    }
+
+    @Composable
+    private fun Greeting(name: String) {
+
+        //remember: to avoid change during recomposing
+        val expanded = remember { mutableStateOf(false) }
+
+        val extraPadding = if (expanded.value) 48.dp else 0.dp
+
+        Surface(
+            shape = MaterialTheme.shapes.small,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+        ) {
+            Row(modifier = Modifier.padding(24.dp),) {
+                Column(modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+                ) {
+                    Text(text = "Hello, ")
+                    Text(text = name)
+                }
+                Button(
+                    shape = MaterialTheme.shapes.medium,
+                    onClick = { expanded.value = !expanded.value },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.White, contentColor = Color.Blue)
+                ) {
+                    Text(if (expanded.value) "Show less" else "Show more")
+                }
+            }
         }
     }
 }
